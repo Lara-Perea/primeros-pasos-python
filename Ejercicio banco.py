57c8defbba4248483d2b0c48b966bca257cc5de6
Lara-Perea <laraperea2001@hotmail.com>
2026-08-11 22:29:13 -0300

    Update Ejercicio banco.py

--- Ejercicio banco.py
+++ Ejercicio banco.py
@@ -1,13 +1,3 @@
-#---------------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      jazla
-#
-# Created:     11/08/2026
-# Copyright:   (c) jazla 2026
-# Licence:     <your licence>
-#---------------------------------------------------------------------------------------
 #c= capital  i= incremento 6%  m= meses gm= ganancia mensual
 #mf= monto final
 c = float(input("Ingrese el capital a invertir: "))
