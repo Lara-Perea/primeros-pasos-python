b38bc2e32a1b7a4a69b74b2d3a9f0e4c12bad656
Lara-Perea <laraperea2001@hotmail.com>
2026-08-11 23:06:24 -0300

    Delete Ejercicio banco.py

--- Ejercicio banco.py
+++
@@ -1,8 +0,0 @@
-#c= capital  i= incremento 6%  m= meses gm= ganancia mensual
-#mf= monto final
-c = float(input("Ingrese el capital a invertir: "))
-m = float(input("Ingrese los meses a invertir: "))
-i = 0.06
-gm = (c*i)* m
-mf = gm + c
-print("Si usted invierte: ",c, "por: ",m,"meses, el monto final que obtendrá será de: ",mf)
